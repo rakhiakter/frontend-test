bb62c789081518df3b87bac6601334417cbc9d2d
Rakhi Akter <rakhi32799@gmail.com>
2022-10-20 20:38:04 +0600

    nevigation path added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Home from './component/Home/Home';
+import Login from './component/Login/Login';
+import Registration from './component/Registration/Registration';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./component/Header/Header";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        {/* <Routes> */}
+        <Route exact path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        {/* </Routes> */}
+      </Routes>
     </div>
   );
 }
---
+++ src/component/Header/Header.js
@@ -0,0 +1,52 @@
+import React, { useContext } from "react";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Container,
+  Button,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <div>
+      <Container>
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">
+            <Link to="/">
+              <img style={{ width: "120.26px", height: "56px" }} alt="" />
+            </Link>
+          </Navbar.Brand>
+          <Form inline>
+            <FormControl
+              id="search"
+              type="text"
+              placeholder="Search your Destination..."
+              className="mr-sm-2"
+            />
+          </Form>
+
+          <Nav className="ml-auto">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+            <Link className="nav-link" to="/registration">
+              Registration
+            </Link>
+
+            {/* {loggedInUser.email && loggedInUser.email !== "" ? (
+              <button onClick={() => setLoggedInUser({})}>Logout</button>
+            ) : (
+              <Link className="nav-link" to="/login">
+                <Button variant="warning">Login</Button>
+              </Link>
+            )} */}
+          </Nav>
+        </Navbar>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;
